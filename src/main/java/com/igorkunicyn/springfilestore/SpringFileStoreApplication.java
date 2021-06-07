0c25f886f25403af01d31af157e89f9b3203f713
igorkunicyn <igor_kunicyn@inbox.ru>
2021-06-07 09:31:26 +0300

    Initial commit

---
+++ src/main/java/com/igorkunicyn/springfilestore/SpringFileStoreApplication.java
@@ -0,0 +1,13 @@
+package com.igorkunicyn.springfilestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igorkunicyn/springfilestore/SpringFileStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igorkunicyn.springfilestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
